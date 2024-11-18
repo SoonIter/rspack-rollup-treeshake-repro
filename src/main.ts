6878b55793db7aa83ca8557e191b1694ddae443f
wukang <kwu@aibee.com>
2024-11-18 15:05:31 +0800

    feat: test tree sharking

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { BMap } from "@aibee/crc-bmap"
+
+console.log(BMap)
+
+createApp(App).mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // 这里实现 chunk 的拆分
+          if (id.includes("node_modules/@aibee/crc-bmap")) {
+            return "crc-bmap"; // 指定模块名称将会打包成 `crc-bmap.js`
+          }
+        },
+        entryFileNames: "[name].js", // 入口文件名
+        chunkFileNames: "chunks/[name].js", // 抽取 chunk 的文件名
+        assetFileNames: "assets/[name].[ext]", // 资源文件名
+      },
+    },
+  },
+  define: {
+    "process.env.CRC_BMAP_MODE": JSON.stringify("online"),
+  },
+});
